bb9cf4a1f4488df6751cd56b1f1e8017e5528e08
BASV-LAPTOP <venktesh200@gmail.com>
2022-05-21 18:56:38 +0530

    21/05/6.56

---
+++ src/main/java/com/venki/dynamicdevelopment/DynamicdevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.venki.dynamicdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicdevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicdevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/venki/dynamicdevelopment/DynamicdevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venki.dynamicdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicdevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
